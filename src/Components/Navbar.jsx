3dba825a70c52b1c35abac93238f92911c854148
yoshiodei <yoshiodei@yahoo.com>
2021-06-25 11:37:46 +0000

    add Navbar for mobile

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import brand from './../Assets/yoshi_logo.png';
+import './../Assets/css/Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+                    <img src={brand} className="navbar__brand" />
+                    <div className="navbar__burger">
+                        <div className="navbar__burger__inner"></div>
+                        <div className="navbar__burger__inner"></div>
+                        <div className="navbar__burger__inner"></div>
+                    </div>
+                <ul className="navbar__list">
+                    <li className="navbar__list__item" ><a className="navbar__list__link" href="#">About</a></li>
+                    <li className="navbar__list__item" ><a className="navbar__list__link" href="#">Skills</a></li>
+                    <li className="navbar__list__item" ><a className="navbar__list__link" href="#">Projects</a>
+                    </li>
+                    <li className="navbar__list__item" ><a className="navbar__list__link" href="#">Contact</a></li>
+                </ul>
+        </div>
+    );
+}
+
+export default Navbar;
