cb370bd347a4eb09a33e90e64a73dea32554a735
yoshiodei <yoshiodei@yahoo.com>
2021-06-28 19:17:16 +0000

    modify app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import IntroBody from './Components/IntroBody';
 import About from './Components/About';
 import MySkills from './Components/MySkills';
 import Project from './Components/Project';
+import Quote from './Components/Quote';
 
 import './App.css';
 
@@ -14,6 +15,7 @@ function App() {
       <About />
       <MySkills />
       <Project />
+      <Quote />
     </div>
   );
 }
